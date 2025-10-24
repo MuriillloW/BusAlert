771e40f8aa7626f5271c802b405e795a496306ec
MrJosh180 <jjj.josuealmeida@gmail.com>
2025-10-23 21:40:09 -0300

    Página de Home e Favoritos Co-authored-by: MuriillloW <MuriillloW@users.noreply.github.com>

--- src/app/favoritos/favoritos.page.ts
+++ src/app/favoritos/favoritos.page.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonButtons ,IonButton, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, 
+  IonCardContent, IonModal, ActionSheetController  } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { home, person, star, close } from 'ionicons/icons'
+import { NavController } from '@ionic/angular/standalone';
+
+
 
 @Component({
   selector: 'app-favoritos',
   templateUrl: './favoritos.page.html',
   styleUrls: ['./favoritos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonFooter, IonButtons ,IonButton, IonIcon, IonCard, IonCardHeader, IonCardTitle, 
+    IonCardSubtitle, IonCardContent, IonModal  ]
 })
 export class FavoritosPage implements OnInit {
+  isModalOpen = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
   }
 
+  
+
+  constructor(private navCTRL: NavController) { }
+  goHome(){
+    this.navCTRL.navigateRoot('/home');
+  }
+
+  goFavoritos(){
+    this.navCTRL.navigateRoot('/favoritos');
+  }
+
+  goUser(){
+    this.navCTRL.navigateRoot('/user');
+  }
+
+  
+  ngOnInit() {}
+
 }
+addIcons ({ home, person, star, close})
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonButtons ,IonButton, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, 
+  IonCardContent, IonModal, ActionSheetController } from '@ionic/angular/standalone';
+import { NavController } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { home, person, star, close } from 'ionicons/icons'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonButtons ,IonButton, IonIcon, IonCard, IonCardHeader, IonCardTitle, 
+    IonCardSubtitle, IonCardContent, IonModal  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private navCTRL: NavController) { }
+  goHome(){
+    this.navCTRL.navigateRoot('/home');
+  }
+
+  goFavoritos(){
+    this.navCTRL.navigateRoot('/favoritos');
+  }
+
+  goUser(){
+    this.navCTRL.navigateRoot('/user');
+  }
 }
+
+addIcons ({ home, person, star, close})
