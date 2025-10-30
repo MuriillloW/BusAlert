dfea286e0b0db04d2767b6066340b7a8ab1fd818
MrJosh180 <jjj.josuealmeida@gmail.com>
2025-10-30 19:13:49 -0300

    userPage e edição. (não finalizado

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonButtons ,IonButton, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, 
-  IonCardContent, IonModal, ActionSheetController, IonTab, IonTabs, IonTabBar, IonTabButton  } from '@ionic/angular/standalone';
+  IonCardContent, IonModal, IonInput, IonItem, IonLabel, ActionSheetController, IonTab, IonTabs, IonTabBar, IonTabButton  } from '@ionic/angular/standalone';
 import { NavController } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { home, person, star, close, createOutline } from 'ionicons/icons'
@@ -14,10 +14,14 @@ import { home, person, star, close, createOutline } from 'ionicons/icons'
   templateUrl: './user.page.html',
   styleUrls: ['./user.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonButtons ,IonButton, IonIcon, IonCard, IonCardHeader, IonCardTitle, 
-    IonCardSubtitle, IonCardContent, IonModal, IonTab, IonTabs, IonTabBar, IonTabButton ]
+  imports: [CommonModule, FormsModule, IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonButtons ,IonButton, IonIcon, IonCard, IonCardHeader, IonCardTitle, 
+    IonCardSubtitle, IonCardContent, IonModal, IonInput, IonItem, IonLabel, IonTab, IonTabs, IonTabBar, IonTabButton ]
 })
 export class UserPage implements OnInit {
+
+  isModalOpen = false;
+  user: { name: string; email: string } = { name: 'Nome', email: 'exemplo@exemplo.com' };
+  editUser: { name: string; email: string } = { ...this.user };
   
 
 
@@ -38,6 +42,27 @@ export class UserPage implements OnInit {
 
   goUser(){
     this.navCTRL.navigateRoot('/user');
+  }
+
+  openEditModal(){
+    // prepara os dados para edição
+    this.editUser = { ...this.user };
+    this.isModalOpen = true;
+  }
+
+  closeEditModal(){
+    this.isModalOpen = false;
+  }
+
+  saveProfile(){
+    // validação mínima
+    if(!this.editUser.name || !this.editUser.email){
+      window.alert('Preencha nome e email.');
+      return;
+    }
+    this.user = { ...this.editUser };
+    this.isModalOpen = false;
+    window.alert('Perfil atualizado.');
   }
 }
 
