78adf957766d072ebd2c0150b11b46eeaa2041d2
Ana Beatriz Arguelho <106892550+anabeatrizarguelho@users.noreply.github.com>
2025-11-06 01:37:04 -0300

    Ocultando API Key environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-  apiKey: "AIzaSyB1OojHhrjQEqdezkeACLSv0EPZEekiUvY",
-  authDomain: "busalert-e4d7b.firebaseapp.com",
-  projectId: "busalert-e4d7b",
-  storageBucket: "busalert-e4d7b.firebasestorage.app",
-  messagingSenderId: "800606447313",
-  appId: "1:800606447313:web:503ee94023c0b5e91e07f0"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 }
 };
 
