a9d8ee5c62486977f9953fd55a7745919e0025f2
MrJosh180 <jjj.josuealmeida@gmail.com>
2025-11-14 19:52:39 -0300

    conflitos resolvidos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+// Caso a inicialização do Firebase seja necessária em produção, defina aqui o firebaseConfig
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig
 };
